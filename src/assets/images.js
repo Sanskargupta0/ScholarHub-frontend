318bca8b30a3958a7a429a7ab0dac6d397d924f2
Aman Raj <igamanraj>
2024-09-21 12:22:19 +0530

    Added: images in home page

---
+++ src/assets/images.js
@@ -0,0 +1,56 @@
+import hero_bg from './images/hero-bg.svg';
+import hero_banner_1 from './images/hero-banner-1.jpg';
+import hero_banner_2 from './images/hero-banner-2.jpg';
+import hero_shape_1 from './images/hero-shape-1.svg';
+import hero_shape_2 from './images/hero-shape-2.png';
+import category_1 from './images/category-1.svg';
+import category_2 from './images/category-2.svg';
+import category_3 from './images/category-3.svg';
+import category_4 from './images/category-4.svg';
+import about_banner from './images/about-banner.jpg';
+import about_shape_1 from './images/about-shape-1.svg';
+import about_shape_2 from './images/about-shape-2.svg';
+import about_shape_3 from './images/about-shape-3.png';
+import about_shape_4 from './images/about-shape-4.svg';
+import course_1 from './images/course-1.jpg';
+import course_2 from './images/course-2.jpg';
+import course_3 from './images/course-3.jpg';
+import video_bg from './images/video-bg.png';
+import video_banner from './images/video-banner.jpg';
+import video_shape_1 from './images/video-shape-1.png';
+import video_shape_2 from './images/video-shape-2.png';
+import blog_bg from './images/blog-bg.svg';
+import blog_1 from './images/blog-1.jpg';
+import blog_2 from './images/blog-2.jpg';
+import blog_3 from './images/blog-3.jpg';
+import blog_shape from "./images/blog-shape.png";
+
+export default {
+    hero_bg,
+    hero_banner_1,
+    hero_banner_2,
+    hero_shape_1,
+    hero_shape_2,
+    category_1,
+    category_2,
+    category_3,
+    category_4,
+    about_banner,
+    about_shape_1,
+    about_shape_2,
+    about_shape_3,
+    about_shape_4,
+    course_1,
+    course_2,
+    course_3,
+    video_bg,
+    video_banner,
+    video_shape_1,
+    video_shape_2,
+    blog_bg,
+    blog_1,
+    blog_2,
+    blog_3,
+    blog_shape,
+
+}
