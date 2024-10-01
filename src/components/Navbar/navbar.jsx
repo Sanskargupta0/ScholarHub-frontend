3cea1a02946c0f5ac6922792603a027b73b794c8
Sanskargupta0 <sanskar362002@gmail.com>
2024-10-01 10:12:00 +0530

    Updated: Css and add social login button

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -3,6 +3,7 @@ import "./navbar.css";
 import images from "../../assets/images";
 import { Link } from "react-router-dom";
 import DarkMode from "../DarkMode/DarkMode";
+import { components } from "../";
 
 function Navbar() {
   useEffect(() => {
@@ -37,58 +38,56 @@ function Navbar() {
       form.classList.add("active");
       searchBtn.classList.add("hide");
       cancelBtn.classList.add("show");
-    }; 
-
+    };
   }, []);
 
   return (
     <div className="navbarStyles" style={{ backgroundColor: "#171C24" }}>
       <nav className="container">
-        <div className="menu-icon">
-          <span className="fas fa-bars"></span>
+        <div className="flex items-center mobileresponsive">
+          <div className="menu-icon">
+            <span className="fas fa-bars"></span>
+          </div>
+          <img
+            src={images.logo}
+            alt="logo"
+            className="imagemobile"
+          />
+
+          <div className="logo mx-1.5">ScholarHub</div>
+          <div className="nav-items">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/About">About</Link>
+            </li>
+            <li>
+              <Link to="/Contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/Library">Library</Link>
+            </li>
+            <li>
+              <Link to="/ExamPaper">Exam Paper</Link>
+            </li>
+          </div>
         </div>
-        <img src={images.logo} alt="logo" style={{ height: "40px" }}/>
-        
-        <div className="logo">ScholarHub</div>
-        <div className="nav-items">
-          <li >
-            <Link to="/">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/About">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="/Contact">
-              Contact
-            </Link>
-          </li>
-          <li>
-            <Link to="/Library">
-              Library
-            </Link>
-          </li>
-          <li>
-            <Link to="/ExamPaper">
-              Exam Paper
-            </Link>
-          </li>
-          
+        <div className="flex items-center mobileresponsive2">
+          <div className="mx-4">
+            <components.Button />
+          </div>
+          <div className="darkMode">
+            <DarkMode />
+          </div>
+          <div className="search-icon">
+            <span className="fas fa-search"></span>
+          </div>
+          <div className="cancel-icon">
+            <span className="fas fa-times"></span>
+          </div>
         </div>
-        <div className="justincreasingsizeformobile"></div>
-        <div className="darkMode">
-          <DarkMode />
-        </div>
-        <div className="search-icon">
-          <span className="fas fa-search"></span>
-        </div>
-        <div className="cancel-icon">
-          <span className="fas fa-times"></span>
-        </div>
-        <form action="#" style={{"marginLeft":"1rem"}}>
+        <form action="#" style={{ marginLeft: "1rem" }}>
           <input
             type="search"
             className="search-data"
--- src/components/components.js
+++ src/components/components.js
@@ -1,9 +1,11 @@
 import Navbar from './Navbar/navbar';
 import Footer from './Footer/footer';
 import Contributor from "./Contributor/Contributor";
+import Button from "./Button/Button";
 
 export default {
     Navbar,
     Footer,
-    Contributor
+    Contributor,
+    Button
 };   
